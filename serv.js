33d95e555580f63fc85383b1a68de10b553e065e
Dmitry Mironov (ICBRK) <dimamironov1337228@gmail.com>
2023-02-09 00:01:13 +0500

    xuita kakaya-to

---
+++ serv.js
@@ -0,0 +1,17 @@
+let a = 5
+let b = 7
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns sum
+ */
+
+let sum = (a,b) => a + b
+
+let div = document.createElement('div')
+div.innerHTML = `Sum is ${sum(a,b)}`
+Element.body.appendChild(div)
+
+
